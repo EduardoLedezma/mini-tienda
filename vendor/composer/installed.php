3b77b4fee86a5570b9e2b33a18e0d73571ddb76d
EduardoLedezma <laloledezma3@hotmail.com>
2025-12-02 21:22:06 -0600

    Fix: Configuracion para Heroku con composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '26abd5fd379d5755c404f38b930e66995d13c3e2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '26abd5fd379d5755c404f38b930e66995d13c3e2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
